e76fbb91054a7330ae674e03f901727630922d69
RiazK <RiazCKhan@gmail.com>
2022-03-17 12:03:54 -0400

    Add Roman Numeral Symbol - Value Variables

---
+++ roman-to-integer.js
@@ -0,0 +1,8 @@
+// Roman Numeral Symbol = Value
+const romanNumI = 1
+const romanNumV = 5
+const romanNumX = 10
+const romanNumL = 50
+const romanNumC = 100
+const romanNumD = 500
+const romanNumM = 1000
