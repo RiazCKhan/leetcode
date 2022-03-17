8cae5675bba985c032c41215fd2480ed5bfb11aa
RiazK <RiazCKhan@gmail.com>
2022-03-17 12:18:55 -0400

    Example 1 complete, simple count

--- roman-to-integer.js
+++ roman-to-integer.js
@@ -1,8 +1,38 @@
-// Roman Numeral Symbol = Value
-const romanNumI = 1
-const romanNumV = 5
-const romanNumX = 10
-const romanNumL = 50
-const romanNumC = 100
-const romanNumD = 500
-const romanNumM = 1000
+const romanToInt = (string) => {
+
+  const stringArr = string.split('');
+
+  let romanNumI = 0;
+  let romanNumV = 0;
+  let romanNumX = 0;
+  let romanNumL = 0;
+  let romanNumC = 0;
+  let romanNumD = 0;
+  let romanNumM = 0;
+
+  for (const letter of stringArr) {
+
+    if (letter === "I") {
+      romanNumI += 1
+    }
+
+  }
+
+  console.log(romanNumI)
+
+};
+
+const s = "III";
+
+romanToInt(s);
+
+/*
+
+Input = "String"
+
+1. Split String into Arr
+2. Loop throug Arr
+
+Output = "Number"
+
+*/
