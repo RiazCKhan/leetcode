b307259eca471001578266bd4d3650393156ca4e
RiazK <RiazCKhan@gmail.com>
2022-03-18 14:28:02 -0400

    Create twoSum FN + FN call with mock data

---
+++ two-sum.js
@@ -0,0 +1,13 @@
+// Find 2 numbers in an Arr that = target num
+// Input = Arr, target = int
+// Output = Arr corresponding the the value position = target
+
+// nums = arr
+
+const twoSum = (nums, target) => {
+
+
+
+};
+
+twoSum([2, 7, 11, 15], 9)
