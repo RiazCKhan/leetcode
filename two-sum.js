e75dab50422edd761d02bfdc268d62a8488dff80
RiazK <RiazCKhan@gmail.com>
2022-03-18 15:10:37 -0400

    Implement nested for loop

--- two-sum.js
+++ two-sum.js
@@ -6,8 +6,24 @@
 
 const twoSum = (nums, target) => {
 
+  let result = [];
 
+  for (let i = 0; i <= nums.length; i++) {
+    console.log('this is outter i', i)
+    for (let j = i + 1; j <= nums.length; j++) {
+      console.log('this is inner j', j)
+      if (nums[i] + nums[j] === target) {
+        // result.push(nums[i])
+        // result.push(nums[j])
+      }
+    }
+  }
 
+  console.log(result)
 };
 
 twoSum([2, 7, 11, 15], 9)
+
+
+// take first element
+// add every element to it to see if  = target 
