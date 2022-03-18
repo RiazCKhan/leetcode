f85d106056dd7a205ae979f469344505c185a46d
RiazK <RiazCKhan@gmail.com>
2022-03-18 17:32:30 -0400

    Remove forloop console logs contributing to output limit error

--- two-sum.js
+++ two-sum.js
@@ -1,17 +1,9 @@
-// Find 2 numbers in an Arr that = target num
-// Input = Arr, target = int
-// Output = Arr corresponding the the value position = target
-
-// nums = arr
-
 const twoSum = (nums, target) => {
 
   let result = [];
 
   for (let i = 0; i <= nums.length; i++) {
-    console.log('this is outter i', i);
     for (let j = i + 1; j <= nums.length; j++) {
-      console.log('this is inner j', j);
       if (nums[i] + nums[j] === target) {
         result.push(i);
         result.push(j);
@@ -21,8 +13,8 @@ const twoSum = (nums, target) => {
   return result;
 };
 
-twoSum([2, 7, 11, 15], 9);
+console.log(twoSum([2, 7, 11, 15], 9));
 
+console.log(twoSum([3, 2, 4], 6));
 
-// take first element
-// add every element to it to see if  = target
+console.log(twoSum([3, 3], 6));
