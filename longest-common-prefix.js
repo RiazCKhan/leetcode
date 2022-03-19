7d391e79cf25deeb48e444c367728eb39aacdd27
RiazK <RiazCKhan@gmail.com>
2022-03-19 16:49:31 -0400

    Incoporate obj map

--- longest-common-prefix.js
+++ longest-common-prefix.js
@@ -1,39 +1,14 @@
 const longestCommonPrefix = function (strs) {
 
-// check if first letter is the same
-// if same push to arr
+  // check if first letter is the same
+  // if same push to arr
 
-let arr = [];
-
-  for (let i = 0; i <= strs.length; i++) {
-   
-    console.log(strs[i])
-
-  }
+  let obj = {};
 
 
-  // let prefixArr = [];
-  // let result = [];
 
-  // strs.forEach((str) => {
-  //   if (!prefixArr.includes(str.substring(0, 1)))
-  //     prefixArr.push(str.substring(0, 1));
-  // });
-
-  // let prefixStr = prefixArr.join('');
-
-  // strs.forEach((str) => {
-  //   if (prefixStr === str.substring(0, 1)) {
-  //     result.push(prefixStr);
-  //   }
-  // });
-
-  // if (result.length === 0) {
-  //   return "";
-  // } else {
-  //   return result[0];
-  // }
-
+  console.log(arr)
+  return
 };
 
 longestCommonPrefix(["flower", "flow", "flight"]);
