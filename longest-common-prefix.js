566dc423fb026e42771950ab4703f9ad49f00058
RiazK <RiazCKhan@gmail.com>
2022-03-19 11:29:26 -0400

    Complete challenge

--- longest-common-prefix.js
+++ longest-common-prefix.js
@@ -9,6 +9,23 @@
 
 const longestCommonPrefix = function(strs) {
 
-  
-    
+  let prefixArr = [];
+  let result = [];
+
+  strs.forEach((str) => {
+    if (!prefixArr.includes(str.substring(0, 2)))
+      prefixArr.push(str.substring(0, 2));
+  });
+
+  let prefixStr = prefixArr.join('');
+
+  strs.forEach((str) => {
+    if (prefixStr === str.substring(0, 2)) {
+      result.push(prefixStr);
+    }
+  });
+
+  return result[0];
 };
+
+console.log(longestCommonPrefix(["flower", "flow", "flight"]));
