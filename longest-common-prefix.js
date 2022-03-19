d44415fd329d2809e1de94aafd10f50156a67352
RiazK <RiazCKhan@gmail.com>
2022-03-19 17:09:41 -0400

    Account for edge case return

--- longest-common-prefix.js
+++ longest-common-prefix.js
@@ -6,12 +6,14 @@ const longestCommonPrefix = function (strs) {
   let result = "";
 
   for (let i = 0; i < strs.length; i++) {
-    if (strs[0].charAt(i) === strs[i].charAt(i)) {
+    if (strs[0].charAt(i) !== strs[i].charAt(i)) {
+      return "";
+    } else if (strs[0].charAt(i) !== strs[i].charAt(i)) {
       result += strs[i].charAt(i)
     }
   }
-console.log(result)
+  return result
 };
 
-longestCommonPrefix(["flower", "flow", "flight"]);
-// console.log(longestCommonPrefix(["dog", "racecar", "car"]));
+// longestCommonPrefix(["flower", "flow", "flight"]);
+console.log(longestCommonPrefix(["dog", "racecar", "car"]))
