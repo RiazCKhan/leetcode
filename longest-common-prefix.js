b8375629058adbaea7d57fdca8335ac5c31946ae
RiazK <RiazCKhan@gmail.com>
2022-03-19 11:36:33 -0400

    Update definition of 'prefix' according to test cases

--- longest-common-prefix.js
+++ longest-common-prefix.js
@@ -4,19 +4,25 @@ const longestCommonPrefix = function(strs) {
   let result = [];
 
   strs.forEach((str) => {
-    if (!prefixArr.includes(str.substring(0, 2)))
-      prefixArr.push(str.substring(0, 2));
+    if (!prefixArr.includes(str.substring(0, 1)))
+      prefixArr.push(str.substring(0, 1));
   });
 
   let prefixStr = prefixArr.join('');
 
   strs.forEach((str) => {
-    if (prefixStr === str.substring(0, 2)) {
+    if (prefixStr === str.substring(0, 1)) {
       result.push(prefixStr);
     }
   });
 
-  return result[0];
+  if (result.length === 0) {
+    return "";
+  } else {
+    return result[0];
+  }
+
 };
 
-longestCommonPrefix(["flower", "flow", "flight"])
+console.log(longestCommonPrefix(["flower", "flow", "flight"]));
+console.log(longestCommonPrefix(["dog", "racecar", "car"]));
