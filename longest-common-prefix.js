115c9dfa48a421f3d488c0f0e6eba8171db3a452
RiazK <RiazCKhan@gmail.com>
2022-03-19 11:00:24 -0400

    inital commit

---
+++ longest-common-prefix.js
@@ -0,0 +1,14 @@
+// identify 'prefix'
+// what is a prefix, common string at the begining of the world
+
+// input = arr
+// output = str
+
+// potential tools
+// match, includes, substr, split, indexOf
+
+const longestCommonPrefix = function(strs) {
+
+  
+    
+};
