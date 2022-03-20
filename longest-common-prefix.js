ae5efdb045d9e3a3538cf6ecb98516beab5ef81a
RiazK <RiazCKhan@gmail.com>
2022-03-20 18:49:41 -0400

    Review edge case handling

--- longest-common-prefix.js
+++ longest-common-prefix.js
@@ -1,24 +1,55 @@
-const longestCommonPrefix = function (strs) {
-
-  // check if first letter is the same
-  // if same push to arr
+const longestCommonPrefix2 = function (strs) {
 
   let result = "";
 
-  if (strs[0].substring(0, 1) !== strs[1].substring(0, 1)) {
-    return ""
-  }
+  // how to check IF no common prefix exists
+  // return empty string if non exists
+
+  const emptyIndex = strs.findIndex(str => {
+
+    // console.log('what am i', str) //
+    // console.log("not found -1", str === "")
+
+    return str === ""
+  });
+
 
   for (let i = 0; i < strs.length; i++) {
-    // console.log('prefix', strs[0].charAt(i))
-    // console.log('compare', strs[i].charAt(i))
     if (strs[0].charAt(i) === strs[i].charAt(i)) {
       result += strs[i].charAt(i)
     }
   }
-  // console.log('result', result)
   return result
 };
 
-// longestCommonPrefix(["flower", "flow", "flight"]);
-longestCommonPrefix(["dog", "racecar", "car"])
+
+var longestCommonPrefix = function (strs) {
+
+  // If any str in strs has no index, end run
+  const emptyIndex = strs.findIndex(str => {
+    // console.log('what am i', str) //
+    // console.log("not found -1", str === "")
+    return str === ""
+  });
+
+  if (strs.length === 0 || emptyIndex !== -1) {
+    return "";
+  }
+  let commonFirstChar = "";
+  for (i = 0; i < strs.length; i++) {
+    const firstChar = strs[i].substring(0, 1);
+    if (commonFirstChar === "") {
+      commonFirstChar = firstChar;
+    } else {
+      if (firstChar !== commonFirstChar) {
+        return "";
+      }
+    }
+  }
+  const newStrs = strs.map(item => item.substring(1));
+  return commonFirstChar + longestCommonPrefix(newStrs);
+};
+
+
+longestCommonPrefix(["flower", "flow", "flight"])
+// longestCommonPrefix(["dog", "racecar", "car"])
