046ca21bf64285c128f4e99712afcc2a42742656
RiazK <RiazCKhan@gmail.com>
2022-03-21 20:28:50 -0400

    Pass test1

--- valid-parentheses.js
+++ valid-parentheses.js
@@ -1,9 +1,27 @@
 const isValid = function(s) {
     
+  let inputArr = s.split('')
+
+  for (let i = 0; i <= inputArr.length; i++) {
+
+    if (inputArr[i] === '[') {
+      if (inputArr[i + 1] === ']') {
+        return true
+      }
+    }
+
+
+  }
 
 
 
 };
+
+// Solution Brain Storm
+// Input = string
+// Output = boolean
+// Split Str to get individual 'items'
+//
 
 // TEST CASES
 // Happy Path --> return true
@@ -17,3 +35,5 @@ let test5 = "}{"
 let test6 = "[](]"
 let test7 = "{)[]()"
 let test8 = ""
+
+isValid(test1);
