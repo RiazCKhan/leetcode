44f6b029f95d66accf1cf121131b0dff516aa54e
RiazK <RiazCKhan@gmail.com>
2022-03-21 20:17:38 -0400

    Note possible test cases

---
+++ valid-parentheses.js
@@ -0,0 +1,19 @@
+const isValid = function(s) {
+    
+
+
+
+};
+
+// TEST CASES
+// Happy Path --> return true
+let test1 = "[]"
+let test2 = "[]()"
+let test3 = "[](){}"
+
+// Unhappy --> return false
+let test4 = "(]"
+let test5 = "}{"
+let test6 = "[](]"
+let test7 = "{)[]()"
+let test8 = ""
