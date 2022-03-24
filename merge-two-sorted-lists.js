d3f94f3e38c51bf89ce98a8918536f09883abf27
RiazK <RiazCKhan@gmail.com>
2022-03-23 20:54:31 -0400

    Create strategy

--- merge-two-sorted-lists.js
+++ merge-two-sorted-lists.js
@@ -1,4 +1,16 @@
 const mergeTwoLists = function(list1, list2) {
+
+
+
+
     
 };
 
+// PLAN
+// forEach each List
+// Push each item onto result arr
+// result.sort
+// return result
+
+let arr1 = [1, 2, 3]
+let arr2 = [1, 2, 4]
