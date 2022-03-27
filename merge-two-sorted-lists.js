10bf483ec7d124c5df506dcf006fb1f38f106f36
RiazK <RiazCKhan@gmail.com>
2022-03-27 09:57:04 -0400

    Original solution not possible as arr !== linked list

--- merge-two-sorted-lists.js
+++ merge-two-sorted-lists.js
@@ -1,3 +1,28 @@
+// const mergeTwoLists = function (list1, list2) {
+
+//   if (list1.length === 0 && list2.length ===0){
+//     return [];
+//   }
+
+//   let result = [];
+
+//   let arrOne = Array.from(list1)
+//   let arrTwo = Array.from(list2)
+
+//   for (let i = 0; i < arrOne.length; i++) {
+//     result.push(arrOne[i])
+//   }
+
+//   for (let i = 0; i < arrTwo.length; i++) {
+//     result.push(arrTwo[i])
+//   }
+
+//   result.sort((a, b) => a - b);
+
+//   return result;
+// };
+
+
 const mergeTwoLists = function (list1, list2) {
 
   if (list1.length === 0 && list2.length ===0){
@@ -21,7 +46,6 @@ const mergeTwoLists = function (list1, list2) {
 
   return result;
 };
-
 
 let arr1 = [1, 2, 4];
 let arr2 = [1, 3, 4];
