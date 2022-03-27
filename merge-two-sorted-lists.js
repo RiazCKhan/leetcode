367593b259a2a9b12473c8cba5bb1320943538e3
RiazK <RiazCKhan@gmail.com>
2022-03-27 09:59:54 -0400

    Comment-Review  on solution from discussion board for understanding

--- merge-two-sorted-lists.js
+++ merge-two-sorted-lists.js
@@ -22,29 +22,32 @@
 //   return result;
 // };
 
-
-const mergeTwoLists = function (list1, list2) {
-
-  if (list1.length === 0 && list2.length ===0){
-    return [];
+var mergeTwoLists = function(l1, l2) {
+  // If there are no more items in linked list 1, return the rest of list2
+  if (!l1) {
+    return l2;
+  }
+  // If there are no more items in list2, return the rest of list1
+  if (!l2) {
+    return l1;
   }
 
-  let result = [];
+  // Assign reference values
+  const val1 = l1.val;
+  const val2 = l2.val;
 
-  let arrOne = Array.from(list1)
-  let arrTwo = Array.from(list2)
+  // Create var to store sorted merged lists
+  let mergedNode;
 
-  for (let i = 0; i < arrOne.length; i++) {
-    result.push(arrOne[i])
+  // Compare var values
+  if (val1 < val2) {
+    mergedNode = new ListNode(val1);
+    mergedNode.next = mergeTwoLists(l1.next, l2);
+  } else {
+    mergedNode = new ListNode(val2);
+    mergedNode.next = mergeTwoLists(l2.next, l1);
   }
-
-  for (let i = 0; i < arrTwo.length; i++) {
-    result.push(arrTwo[i])
-  }
-
-  result.sort((a, b) => a - b);
-
-  return result;
+  return mergedNode;
 };
 
 let arr1 = [1, 2, 4];
