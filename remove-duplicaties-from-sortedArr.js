afcaca36f1f3c671d0595f6cb15f254835840b95
RiazK <RiazCKhan@gmail.com>
2022-04-13 13:21:31 -0400

    Add conditional

--- remove-duplicaties-from-sortedArr.js
+++ remove-duplicaties-from-sortedArr.js
@@ -3,11 +3,18 @@
 var removeDuplicates = function(nums) {
     
   for (let i = 0; i < nums.length; i++) {
-    
-    
+    for (let j = 0; j < nums.length; j++) {
+     
+      if (nums[i] === nums[j]) {
+        console.log('hs')
+      }
 
+    }
+    console.log(nums)
   }
 
 };
 
 let input = [0,0,1,1,1,2,2,3,3,4]
+
+removeDuplicates(input)
